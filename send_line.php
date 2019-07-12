d599ffcdfce2184a4564be07a7e2c1de26994aac
fm-mis <52604987+fm-mis@users.noreply.github.com>
2019-07-12 13:39:16 +0000

    Initial commit

---
+++ send_line.php
@@ -0,0 +1,49 @@
+<?php
+ $token = "XDh657bnZH48lPqyaeJdc5vsmUY9HCF3tdzkLVx7AxR";
+
+ $mes = $_GET['mes'];
+ $stickerPkg = 3; //stickerPackageId
+$stickerId = 240; //stickerId
+
+ define('LINE_API',"https://notify-api.line.me/api/notify");  
+
+ $res = notify_message($mes,$stickerPkg,$stickerId,$token);
+
+ print_r($res);
+
+ function notify_message($message,$stickerPkg,$stickerId,$token){
+
+  $data = array('message' => $message,
+  'stickerPackageId'=>$stickerPkg,
+      'stickerId'=>$stickerId);
+
+  $data = http_build_query($data,'','&');
+
+  $header = array( 
+
+          'http'=>array(
+
+             'method'=>'POST',
+
+             'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+
+                       ."Authorization: Bearer ".$token."\r\n"
+
+                       ."Content-Length: ".strlen($data)."\r\n",
+
+             'content' => $data
+
+          ),
+
+  );
+
+  $context = stream_context_create($header);
+
+  $result = file_get_contents(LINE_API,FALSE,$context);
+
+  $res = json_decode($result);
+
+  return $res;
+
+ }
+ ?>
